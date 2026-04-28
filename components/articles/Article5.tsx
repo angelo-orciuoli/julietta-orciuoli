7d05bec80fda0b705d4d18e3ebdd9381d0a45171
Angelo Orciuoli <arorciuoli@gmail.com>
2026-04-28 18:41:39 -0400

    updated articles1

---
+++ components/articles/Article5.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import ArticleLayout from './ArticleLayout';
+
+const Article5: React.FC = () => {
+    return (
+        <ArticleLayout
+            date="October 29, 2024"
+            title="How Nazish Qureshi believes that Climate activism is God's plan for her"
+        >
+            <p>
+                Nazish Qureshi was on her lunch break when a coworker came up to her to show her an article that had been published recently. She grabbed the article and started reading. It was about the intense fracking and earthquakes that had been going on in British Columbia. It was at this very moment Qureshi knew she had to quit her job and make a change.
+            </p>
+
+            <br />
+
+            <p>
+                Qureshi, a native Canadian, went to the University of Calgary and got her Bachelor's degree in energy management. It was there where her love for the natural world started to develop. “I've been exposed to growing up near a lot of natural beauties. That definitely played a part in just having this appreciation for the beautiful world,” Qureshi said in a Zoom interview.
+            </p>
+
+            <br />
+
+            <p>
+                However, after she graduated she went to work for an engineering firm that worked with energy companies in the fossil fuel industry. According to Qureshi the main industry in Alberta at the time was fossil fuels, so no matter who you were, it was more than likely to get a job within that realm. Even though her personal values did not necessarily align with her job, there wasn’t much choice to begin with.
+            </p>
+
+            <br />
+
+            <p>
+                However, when she read that article about fracking something within her changed. “I felt like something shift inside my heart. I think that was the first time where I could just really clearly see that I was still part of this giant machine that was destroying our home and I really didn't like that feeling,” Qureshi said.
+            </p>
+
+            <br />
+
+            <p>
+                After that she quit her job and went back to the University of Calgary to get a masters degree in sustainable energy development. After that, she worked in the Canadian government helping implement the Alberta climate leadership plan and transition to a lower carbon economy. She was only there for four years because the agency she worked for was shut down after there was  government change in Alberta. “It was great, theoretically, for the potential, like the scale of impact that you can have in a very short amount of time. But at the same time, it was quite unfortunate, because it's politics. And unfortunately, climate, to this day, can be so political and so there was a lot of uncertainty in terms of the longevity of your work,” Qureshi said.
+            </p>
+
+            <br />
+
+            <p>
+                She then moved into environmental education and the non-profit space hoping that she can have the same impact but also be more secure in her role long-term. It was also at this point where Qureshi started to use her Islamic faith to guide her career. She said her religious values always informed her choice of career and industry but this is when she really started to intersect her faith with her non-profit work. “I felt that there isn't a space where Muslims could come together and talk about climate solutions and environmental stewardship without having to sanitize their Muslimness. There's a lot of Muslim professionals in this space, but they don't really engage in faith based climate advocacy,” Qureshi said. This was the reason she started to bring her faith into her environmental work. She started on a smaller scale implementing ‘green teams’ into local mosques and talking to the people there about climate justice.
+            </p>
+
+            <br />
+
+            <p>
+                As she got further into this work she felt like it was “more of a responsibility than an opportunity” to intersect these parts of her life. Although she has never faced any blatant prejudice or bias in this industry because of her faith she said, “Even unconsciously, there's an element of white saviorism in the environmental space, and that has plagued. If you look at a lot of the environmental policies…in the West, be it Canada or America. They have colonial, imperialist roots.” Additionally, she pointed out that climate change disproportionately affects places with a high Muslim population. Whether it be war, displacement, natural disasters etc.
+            </p>
+
+            <br />
+
+            <p>
+                This is when she founded Muslim Climate Watch. A non-profit organization that empowers Muslims to be environmental leaders and advocacy seekers. “We have to do our part in helping those countries in the world that are bearing a disproportionate burden of this climate pollution that [the west] have caused. So, as Americans and Muslims, part of this is broader identity, we have a responsibility to advocate for more policy change,” Qureshi said. She founded this organization in 2023 in hopes to educate Muslims on environmental issues, urge them to take change and seek justice, and use this tight-knit community to push for change in policy and government. 
+            </p>
+
+            <br />
+
+            <p>
+                Qureshi has now been the director of Muslim Climate Watch for over a year and has seen rapid growth in her organization. They have an editorial on their website in which their members and Muslims in their community can write a post about climate issues pertinent to them. They also work closely with Interfaith Power and Light, another climate justice organization that centers faith based advocacy. They are also in the DC Climate Coalition which is a group of climate advocacy organizations that seek local and national climate-friendly solutions. 
+            </p>
+
+            <br />
+
+            <p>
+                Qureshi said that, “My climate work is my worship of God.” She believes that advocating for a cleaner and healthier environment is a way to worship God and show appreciation for his creation. “I want to serve God's creation to the best of my ability. And I think my ability in this equation is understanding climate change and being able to navigate that.”
+            </p>
+
+            <br />
+
+            <p>
+                Qureshi has been in this industry for her entire adult life and plans on to continue her work in it through expanding her organization and mobilizing more Muslims to take part in climate justice work.
+            </p>
+
+            <br />
+
+            <p>
+                “I never questioned the overall purpose of my being, because this is something that I know how to do. This is the knowledge that God has blessed me with. And so it's always been very clear to me that this is my role,” Qureshi said.
+            </p>
+        </ArticleLayout>
+    );
+};
+
+export default Article5;
---
+++ components/articles/Article6.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import ArticleLayout from './ArticleLayout';
+
+const Article6: React.FC = () => {
+    return (
+        <ArticleLayout
+            date="December 10, 2024"
+            title="Climate Organizers plan for a Future Trump Administration"
+        >
+            <p>
+                With Trump set to take office in less than two months, climate activist organizations have to shift gears and adjust their agendas to fit within this new administration. On his campaign trail Trump made comments on how he wants to roll back certain climate policies like the Inflation Reduction Act which has scared climate organizers about what the future of climate looks like.
+            </p>
+
+            <br />
+
+            <p>
+                The Inflation Reduction Act (IRA) was signed into law by President Biden in August of 2022. It is considered one of the most significant climate legislation in American history. The IRA offers funding, programs and incentives for individuals and businesses to move towards clean energy. One of the main parts of this is an investment tax credit regarding renewable energy. The IRA tax credit enables taxpayers to deduct a percentage of what they pay in renewable energy from their federal taxes. For example, if someone were to buy a new plug-in electric vehicle, they may be qualified to get $7,000 in tax credit from the IRS. There are limits to this, if they exceed a certain modified adjusted gross income, they are no longer eligible. But even with certain restrictions, this tax credit creates tax incentives for electric vehicles.
+            </p>
+
+            <br />
+
+            <p>
+                This is only one example of a tax credit listed in the IRA, it has many other tax credits that all incentivize clean and renewable energy usage among American citizens. One of the tax credits that has the potential to impact the most Americans is one regarding home improvement. If someone were to make energy improvements to their home, like getting an electric stovetop, improving the insulation, or improving the doors and windows, tax credits are available to a portion of those renovation expenses.
+            </p>
+
+            <br />
+
+            <p>
+                A poll done by Data for Progress done in August of 2024, asked voters whether or not they supported the IRA. Although the percentage was higher among independents and democrats, the majority of republicans seemed to be in favor of it as well. This shows that although it is a climate issue, the tax credits are something that has appealed to a lot of Americans regardless of party.
+            </p>
+
+            <br />
+
+            <p>
+                Putting these credits into law gives Americans a massive incentive to switch to clean energy as they would save a lot of money in doing so. This is why the IRA is regarded as a game changing piece of legislation in the climate industry. Climate organizers and climate nonprofits viewed it as a massive win when it was signed into law in 2022, but their win was quite short lived as now they have to fight to uphold this piece of legislation.
+            </p>
+
+            <br />
+
+            <p>
+                On his campaign trail Trump constantly criticized the IRA and even said he wanted to repeal it. In September 2024 he said, “It actually sets us back, as opposed to moves us forward. And [I will] rescind all unspent funds under the misnamed Inflation Reduction Act.”
+            </p>
+
+            <br />
+
+            <p>
+                This was a massive blow to climate organizations as they worked so hard to get the IRA to be put into effect in the first place. Although Trump never specified which programs in the IRA he would specifically target, threatening it in the first place was seen as a big step back for climate organizers.
+            </p>
+
+            <br />
+
+            <p>
+                Jessica Nesterak, the Director of Marketing and Communications​​ for the Alliance to Save Energy (ASE) expressed these concerns. “I think everybody is very worried about the IRA and what will happen to that in the Trump administration and what will happen to those tax credits. On the campaign trail the Trump campaign talked about rolling back regulations, limiting the sales of like incandescent light bulbs, and not pushing LED light bulbs. So we definitely would be in favor of protecting things like that. There has been indication, at least from the campaign trail, that the Trump administration wants to roll back those Biden era regulations,” Nesterak said in a Zoom interview.
+            </p>
+
+            <br />
+
+            <p>
+                The ASE is a bipartisan non-profit that pushes for clean and renewable energy policy. Nesterak has been in her position for only a month but can predict the challenges that may be approaching with this new Trump administration.
+            </p>
+
+            <br />
+
+            <p>
+                Nesterak is not only concerned about issues like the roll back of the IRA but also worried that people might be taking steps backwards in terms of language surrounding climate change. She fears with a new Trump administration organizations like hers can’t be as blunt in their climate forward policies and have to rephrase some of their agendas to reach a larger and more diverse crowd.
+            </p>
+
+            <br />
+
+            <p>
+                “If we're talking to a republican congressman we're not really going to resonate well if we're only talking about sustainability or climate change perhaps. So how do you pivot from that? How do you get around that issue by looking at it from a different perspective? So for us it would be saying ‘well there is a direct tie to the economy.’So you can look at it differently that way. It's making those choices to be strategic in your messaging without getting sidetracked. So it's just looking at the same problem from a different angle.It is a game of defense right now, finding a way to keep messaging around policies consistent but looking at it in perhaps a more creative way,” Nesterak said.
+            </p>
+
+            <br />
+
+            <p>
+                Nesterak emphasized that the biggest challenge she foresees in this new Trump administration is rephrasing that language and having to market climate forward solutions in a different way. Although she knows it will be a challenge, Nesterak also said it might be exciting to find new and creative solutions to pre-existing problems. She is excited about creating new language surrounding these issues that will appeal to more Americans, especially conservatives. “The way democrats have been framing climate change conversations, and they're talking about it like, and they’re right, that it's a ticking time bomb, that there is this tipping point. Do we need to change the way we talk about things? How do we talk about climate change in a way that relates it less to a dire consequence to how does this relates back to the price of groceries. And that is a message that will resonate right now. So it's finding different words to say the same thing,” she said.
+            </p>
+
+            <br />
+
+            <p>
+                A different climate organizer, Peter Dugas, expressed something similar in reframing these issues to appeal to a broader audience. “You can think of this as a national security issue where a lot of our rivals have moved really fast in the direction of decarbonizing and moving into renewable energy. If they corner that market that leaves the United States very vulnerable, not just in energy but also national security. There's a lot of ways of marketing this that speaks to conservative thinking,” he said in a Zoom interview.
+            </p>
+
+            <br />
+
+            <p>
+                Dugas is the Northeast Regional Coordinator for Citizens Climate Lobby, a nonpartisan nonprofit focused on national policies that address climate change. Dugas expressed similar concerns as Nesterak in saying that climate organizations will be having to play “defense” in the next four years. 
+            </p>
+
+            <br />
+
+            <p>
+                Dougherty's fear extends beyond just DuPont though, she feels unsafe in Tenleytown as well. "Being a young female anywhere is scary, I carry pepper spray everywhere cause you just never know." She doesn't feel an exaggerated sense of danger in DuPont specifically, even with the high crime rate, she just has an underlying fear wherever she goes.
+            </p>
+
+            <br />
+
+            <p>
+                However, Dugas does still hold out hope that even with a Trump administration there still can be climate-forward policies and actions that take place. Dugas emphasized that the work he and his organization does not only focuses on federal policy and policymakers, but also community members and average Americans. “Lobbying is one arm but it's also grassroots, moving people to know about what they can do and how they can advocate. Grasstops, which are businesses and community leaders, faith group leaders, all of whom we love to bring to lobby meetings. And then building our chapters, so there is a lot of work we can do regardless of who is in the office to make it so the next time something is possible we can move forward,” he said.
+            </p>
+
+            <br />
+
+            <p>
+                Dugas knows that the next four years will be a challenge with many possible setbacks in climate-friendly policy and legislation surrounding clean and renewable energy but he said, ”We don’t want to be sitting around thinking we have to wait for the next opportunity to get something big done.” Even with these challenges he is confident that his organization and many others will be able to advance their agendas and policies and continue to hope and plan for a greener future. 
+            </p>
+        </ArticleLayout>
+    );
+};
+
+export default Article6;
